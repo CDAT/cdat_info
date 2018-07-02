43a241257eac8cf444e9641e3579601247f7c91c
Charles Doutriaux <doutriaux1@llnl.gov>
2018-07-02 14:52:01 -0700

    need to bring defaultThreshold in

--- Lib/__init__.py
+++ Lib/__init__.py
@@ -3,6 +3,7 @@ from .provenance import generateProvenance
 try:  # is testsrunner installed?
     from testsrunner import run_command, get_sampledata_path, download_sample_data_files  # noqa
     from testsrunner import checkImage
+    from testsrunner.checkimage import defaultThreshold
     from .TestRunnerBase import TestRunnerBase
 except Exception:
     pass
